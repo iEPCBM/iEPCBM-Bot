b52ba8bd3034e194508b478fb78a3203ef8d2717
iEPCBM <rishatk87@gmail.com>
2018-08-07 02:01:50 +0500

    Delete start.bat

--- start.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set PATH=%PATH%;D:\programs\Python34
-set PATH=%PATH%;D:\programs\Python34\Scripts
-set PATH=%PATH%;D:\projects\python\wiki\iEPCBM_Bot\PWB\core
-
-python .\PWB\core\pwb.py iEPCBM_Bot.py
-pause
