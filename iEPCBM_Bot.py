6c4ffbddf194906d1d9814356530115c16c08998
Rishat Kagirov <rishatk87@gmail.com>
2019-06-06 15:44:25 +0500

    Update iEPCBM_Bot.py

--- iEPCBM_Bot.py
+++ iEPCBM_Bot.py
@@ -32,7 +32,7 @@
 # doc for program
 '''
     iEPCBM Bot (YaLVA-B1) is Wikipedia bot
-    Copyright (C) 2017, 2018  Rishat Kagirov (iEPCBM)
+    Copyright (C) 2017-2019  Rishat Kagirov (iEPCBM)
     
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
